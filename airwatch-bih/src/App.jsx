b34034cf26118c90043ab21725ab7232270db241
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-13 11:31:36 +0100

    So it was really hard to set up the map , I put the example code from their website to see if it works as a beginning and will add aqi heatmap later on , I need to set up markers and more but for now this bare minimum is a beginning at least

--- airwatch-bih/src/App.jsx
+++ airwatch-bih/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-    <div>
+    <div id="app" style={{height: '100%'}}>
       <AQIMap />
     </div>
   );
--- airwatch-bih/src/components/AQIMap.jsx
+++ airwatch-bih/src/components/AQIMap.jsx
@@ -1,16 +1,17 @@
-import {MapContainer ,TileLayer} from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-export default function AQIMap(){
-    return(
-        <MapContainer 
-        center={[44.2,17.7]} //BiH centar
-        zoom={7}
-        style={{height:"100vh",width:"100%"}}
-    > 
-    <TileLayera
-    attribution='&copy; Openstreetmap contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+export default function AQIMap() {
+  return (
+    <MapContainer
+      center={[44.2, 17.7]} // centar BiH
+      zoom={7}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
     </MapContainer>
-    );
+  );
 }
