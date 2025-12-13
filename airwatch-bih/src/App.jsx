ebcb27ec191d9e8b0769533ec9880ca18afd82fb
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-13 12:49:34 +0100

    Made the height and width of the app.jsc suit me for testing

--- airwatch-bih/src/App.jsx
+++ airwatch-bih/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-    <div id="app" style={{height: '100%'}}>
+    <div id="app" style={{height: '100vh', width: '100vw'}}>
       <AQIMap />
     </div>
   );
