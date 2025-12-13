24a0a46c21e9620adac5162d8b5f9c01e87dcddd
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-13 12:48:58 +0100

    Imported useMap from leaflet , also found a online opensource leaflet plugin for heatmaps called leaflet.heat  and used it to experiment next steps are aqi api and making the aqi heatmap interactive

--- airwatch-bih/src/components/AQIMap.jsx
+++ airwatch-bih/src/components/AQIMap.jsx
@@ -1,6 +1,21 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet.heat";
 
+const points = [
+    [44.979,16.714, 0.5], // Prijedor city 
+    [44.772,17.191, 0.7], // Banja Luka city
+]
+function HeatmapLayer(){
+    const map = useMap();
+
+    map.whenReady(()=> {
+        const heat = L.heatLayer(points, {radius: 25,blur: 15,maxZoom: 17});
+        heat.addTo(map);
+    })
+    return null;
+}
 export default function AQIMap() {
   return (
     <MapContainer
@@ -12,6 +27,7 @@ export default function AQIMap() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
+      <HeatmapLayer />
     </MapContainer>
   );
 }
