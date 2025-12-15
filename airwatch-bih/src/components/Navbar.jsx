bcbe3d64ef9cfd2bcdd5e82c0c16dc02bb04e769
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-15 16:19:14 +0100

    Starded making the components for the frontend to look good , started with navbar

---
+++ airwatch-bih/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+        <nav className="nav">
+        <h3>🇧🇦 BIH Air Quality</h3>
+        <div>
+            <a href="#map">Map</a>
+            <a href="https://aqicn.org" target="_blank">WAQI</a>
+        </div>
+        </nav>
+    );
+}
