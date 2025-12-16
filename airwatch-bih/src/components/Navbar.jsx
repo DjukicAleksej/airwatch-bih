b574d14fc603bb2f8c7ce00841ccf9ec22a3f0c1
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-16 18:57:59 +0100

    I think I have fixed routing finally

--- airwatch-bih/src/components/Navbar.jsx
+++ airwatch-bih/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
+import {Link} from "react-router-dom"
 export default function Navbar(){
     return(
         <div className="topbar">
             <div className="logo">AQI BIH</div>
             <div className="links">
-                <a href="/">Map</a>
-                <a href="/prevention">Prevention</a>
+                <Link to="/">Map</Link>
+                <Link to="/prevention">Prevention</Link>
                 <a href="https://waqi.info" target="_blank">WAQI</a>
             </div>
         </div>
